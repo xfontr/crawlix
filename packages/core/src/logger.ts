40f96fe88bafeca9ae23226283616f2b208f989e
xfontr <xifre@Xifres-MBP.home>
2023-12-11 16:38:59 +0100

    Test logger message functions

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -2,4 +2,12 @@ import pino from "pino";
 
 const logger = pino();
 
+export const warningMessage = (message: string) => {
+  debugger;
+  logger.warn(message);
+  debugger;
+  return "haha";
+};
+export const infoMessage = (message: string) => logger.info(message);
+
 export default logger;
