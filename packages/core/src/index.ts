2b95290dced50d829cda67ad8efed5c5c6eef94c
Xifre Font <xifre.font@mail.schwarz>
2023-12-15 10:10:16 +0100

    Use action hook now with critical actions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,26 +1,30 @@
 import Session from "./helpers/Session";
 import { launch } from "puppeteer";
-import action from "./utils/action";
 import ENVIRONMENT from "./configs/environment";
+import useAction from "./utils/useAction";
 
 const session = Session();
 
 void (async () => {
   const { store } = session.init();
+  const { taskLength, timeout } = store();
 
-  const $a = action(store.taskLength); // TODO: This is risky, how do we make sure we don't use this somewhere else and wrongly set another length?
+  const { $$a, $i } = useAction(taskLength); // TODO: This is risky, how do we make sure we don't use this somewhere else and wrongly set another length?
 
   const browser = await launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await $a(() => page.goto(ENVIRONMENT.baseUrl), 0);
+  await $$a(() => page.goto(ENVIRONMENT.baseUrl));
 
-  await Promise.all([
-    () => page.waitForNavigation({ timeout: store.timeout }),
-    $a(() => page.click("#text_block-2314-7323"), 0.2),
-  ]);
+  // await Promise.all([
+  //   () => page.waitForNavigation({ timeout }),
+  //   $a(() => page.click("#text_block-2314-7323"), 0.2),
+  // ]);
+
+  const items = await $i(() => page.$$(".ct-headline.landingH3"));
+
 
   console.log(page.url());
 
-  session.end();
+  console.log(store());
 })();
