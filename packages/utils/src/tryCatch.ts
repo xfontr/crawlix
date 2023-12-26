9053c06e1371237c348fa03fda0c0eb2458fdeb2
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-12-26 18:01:56 +0100

    Update tryCatch function for better typing

--- packages/utils/src/tryCatch.ts
+++ packages/utils/src/tryCatch.ts
@@ -1,13 +1,50 @@
-import { type PromiseFunction } from "./types/function";
+// import { type PromiseFunction } from "./types/function";
 
-const tryCatch = async <R, T extends PromiseFunction<R> = PromiseFunction<R>>(
+// const tryCatch = async <R, T extends PromiseFunction<R> = PromiseFunction<R>>(
+//   callback: T,
+// ): Promise<[void | Awaited<R>, void | Error]> => {
+//   try {
+//     const response = await callback();
+//     return [response, undefined];
+//   } catch (error) {
+//     return [undefined, error as Error];
+//   }
+// };
+
+// export default tryCatch;
+
+// import { type PromiseFunction } from "./types/function";
+
+// const tryCatch = async <R, T extends PromiseFunction<R> = PromiseFunction<R>>(
+//   callback: T,
+//   ...args: Parameters<T>
+// ): Promise<[void | Awaited<ReturnType<T>>, void | Error]> => {
+//   try {
+//     const response = await callback(...args);
+//     return [response, undefined];
+//   } catch (error) {
+//     return [undefined, error as Error];
+//   }
+// };
+
+// export default tryCatch;
+
+export type CustomFunction<T = any> = (...args: any[]) => T;
+
+export type PromiseFunction<T = any> = CustomFunction<Promise<T>>;
+
+const tryCatch = async <
+  R = any,
+  T extends PromiseFunction<R> = PromiseFunction,
+>(
   callback: T,
-): Promise<[void | Awaited<R>, void | unknown]> => {
+  ...args: Parameters<T>
+): Promise<[R | void, void | Error]> => {
   try {
-    const response = await callback();
+    const response = await callback(...args);
     return [response, undefined];
   } catch (error) {
-    return [undefined, error];
+    return [undefined, error as Error];
   }
 };
 
