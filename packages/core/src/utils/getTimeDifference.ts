740ee89c6aea4a15349525e7701092b25611047a
Xifre Font <xifre.font@mail.schwarz>
2023-12-28 17:23:15 +0100

    Test get time difference function

--- packages/core/src/utils/getTimeDifference.ts
+++ packages/core/src/utils/getTimeDifference.ts
@@ -1,20 +1,23 @@
+const curateDate = (date: Date | string | number | undefined) => {
+  const curatedDate = typeof date !== "object" ? new Date(date ?? "") : date;
+
+  return curatedDate.toString().toLowerCase() === "invalid date"
+    ? 0
+    : curatedDate;
+};
+
 const getTimeDifference = (
-  startDate: Date | string,
-  endDate?: Date | string,
+  startDate: Date | string | number,
+  endDate?: Date | string | number,
 ): number => {
-  const curatedStartDate =
-    typeof startDate === "string" ? new Date(startDate) : startDate;
+  const curatedStartDate = curateDate(startDate);
+  const curatedEndDate = curateDate(endDate);
 
-  const curatedEndDate =
-    typeof endDate === "string" ? new Date(endDate) : endDate;
-
-  if (curatedStartDate.toString().toLowerCase() === "invalid date") return 0;
-
-  if (endDate && curatedStartDate.toString().toLowerCase() === "invalid date")
-    return 0;
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+  if ((endDate && !curatedEndDate) || !curatedStartDate) return 0;
 
   return (
-    (endDate ? curatedEndDate!.getTime() : new Date().getTime()) -
+    (endDate ? (curatedEndDate as Date).getTime() : new Date().getTime()) -
     curatedStartDate.getTime()
   );
 };
