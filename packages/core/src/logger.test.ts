45f5f6d1dcc0ae15ca67bb2d175e9f2e797b9e68
xfontr <xifre@Xifres-MacBook-Pro.local>
2024-01-01 21:51:22 +0100

    Update logger tests

--- packages/core/src/logger.test.ts
+++ packages/core/src/logger.test.ts
@@ -10,12 +10,21 @@ jest.mock("pino", () => () => ({
   error: (message: string) => mockError(message),
 }));
 
+const mockEmit = jest.fn();
+
+jest.mock("./utils/EventBus", () => ({
+  emit: (...args: unknown[]) => mockEmit(...args),
+}));
+
 describe("Given a warningMessage function", () => {
   describe("When called with a 'warning' message", () => {
     test("Then it should call the logger warn with said message", () => {
       const message = "warning";
+
       warningMessage(message);
+
       expect(mockWarn).toHaveBeenCalledWith(message);
+      expect(mockEmit).toHaveBeenCalledWith("SESSION:LOG", message);
     });
   });
 });
@@ -24,8 +33,11 @@ describe("Given a infoMessage function", () => {
   describe("When called with a 'info' message", () => {
     test("Then it should call the logger warn with said message", () => {
       const message = "info";
+
       infoMessage(message);
+
       expect(mockInfo).toHaveBeenCalledWith(message);
+      expect(mockEmit).toHaveBeenCalledWith("SESSION:LOG", message);
     });
   });
 });
@@ -34,8 +46,11 @@ describe("Given a errorMessage function", () => {
   describe("When called with a 'error' message", () => {
     test("Then it should call the logger error with said message", () => {
       const message = "error";
+
       errorMessage(message);
+
       expect(mockError).toHaveBeenCalledWith(message);
+      expect(mockEmit).toHaveBeenCalledWith("SESSION:LOG", message);
     });
   });
 });
