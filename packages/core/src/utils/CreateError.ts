97525ae9dd4256acd36124a4eb263e6139912c4c
xfontr <xifre@Xifres-MacBook-Pro.local>
2024-01-01 18:18:01 +0100

    CreateError function

---
+++ packages/core/src/utils/CreateError.ts
@@ -0,0 +1,25 @@
+import t from "../i18n";
+import CustomError, { type CustomErrorProps } from "../types/CustomError";
+
+const CreateError = (
+  error: Error | CustomError,
+  { name, publicMessage }: CustomErrorProps = {},
+): CustomError => {
+  if (!(error instanceof Error)) {
+    error = new Error(t("misc.unknown_error"));
+  }
+
+  error.name = name ?? error.name ?? t("misc.unknown_error");
+
+  Object.defineProperty(
+    error,
+    "publicMessage",
+    { value: publicMessage } ??
+      (error as CustomError)?.publicMessage ??
+      error.message,
+  );
+
+  return error as CustomError;
+};
+
+export default CreateError;
