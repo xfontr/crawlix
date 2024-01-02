b168f6633b887eb863112413f270c58bb5feb5eb
xfontr <xifre@Xifres-MacBook-Pro.local>
2024-01-02 18:08:12 +0100

    More complex index with sequential promises

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,36 +1,49 @@
+import { LIMIT_PAGES_MAX } from "./configs/session";
 import Scraper from "./services/Scraper";
-import EventBus from "./utils/EventBus";
+
+const ELEMENT_SELECTOR = ".compact-product";
+
+const NEXT_PAGE_BUTTON =
+  "#app > div.v-application--wrap > main > div > div > div > div:nth-child(6) > div > div.col-md-9.col-12 > div > div:nth-child(4) > div > div > nav > ul > li:nth-child(7) > button";
 
 const ITEM_DATA = {
-  title: ".ct-headline.landingH3",
-  categories: ".ct-text-block.etiquetas",
+  title: ".compact-product-title",
+  author: ".compact-product-authors",
 };
 
 void (async () => {
-  const scraper = await Scraper(
-    { usageData: true, allowDefaultConfigs: true },
-    ITEM_DATA,
-  )!;
+  const { run, afterAll } = await Scraper({}, ITEM_DATA)!;
 
-  if (!scraper) return;
+  await run(async ({ scrapItems, changeSPAPage, store }) => {
+    const action = async () => {
+      await scrapItems(ELEMENT_SELECTOR);
+      await changeSPAPage(NEXT_PAGE_BUTTON, ELEMENT_SELECTOR);
+    };
 
-  const { run, afterAll } = scraper;
+    const promiseAllSequentially = async <T>(
+      tasks: (() => Promise<T>)[],
+      breakingCondition: () => boolean,
+    ) => {
+      const results = [];
+      for (const task of tasks) {
+        results.push(await task());
+        if (breakingCondition()) return;
+      }
 
-  await run(async ({ scrapItems, changePage }) => {
-    await scrapItems("#_dynamic_list-2058-7323 > .ct-div-block");
+      return results;
+    };
 
-    await changePage(
-      "#_dynamic_list-2058-7323 > div.oxy-repeater-pages-wrap > div > a:nth-child(2)",
+    await promiseAllSequentially(
+      new Array<() => Promise<void>>(LIMIT_PAGES_MAX).fill(action),
+      () =>
+        store().location.page >= store().limit.page! ||
+        store().totalItems >= store().limit.items!,
     );
-
-    await scrapItems("#_dynamic_list-2058-7323 > .ct-div-block");
   });
 
   await afterAll(async ({ saveAsJson }) => {
     await saveAsJson();
   });
-
-  EventBus.removeAllListeners("SESSION:LOG");
 
   process.exit(0);
 })();
