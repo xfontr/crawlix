6e9aa6ea5f56ab508164b565561500c6e6d1455e
xfontr <xifre@Xifres-MacBook-Pro.local>
2024-01-02 22:27:04 +0100

    Refactor Scraper wrapper to receive external scraping tool

--- packages/core/src/services/Scraper.ts
+++ packages/core/src/services/Scraper.ts
@@ -1,73 +1,74 @@
-import { type DefaultItem, Session, type SessionConfig } from "../..";
+import { Session } from "../..";
 import t from "../i18n";
 import { infoMessage } from "../logger";
-import Puppeteer from "../helpers/Puppeteer";
-import ScraperTools from "./ScraperTools";
 import SessionStore from "../helpers/SessionStore";
 import { tryCatch } from "@personal/utils";
+import type { SessionConfigInit } from "../types/SessionConfig";
 
 type AfterAllTools = Pick<ReturnType<typeof Session>, "notify"> &
   Pick<ReturnType<typeof Session>, "saveAsJson"> &
   Pick<ReturnType<typeof SessionStore>, "logMessage">;
 
-const Scraper = async (
-  baseConfig: Partial<SessionConfig>,
-  itemData?: Partial<Record<keyof DefaultItem, string>>,
-) => {
-  // INIT - The order of the following tasks is important
-  const $s = Session(baseConfig).init();
+const scraper = <S extends Record<string, unknown>>({
+  ScraperTool,
+  ...baseConfig
+}: SessionConfigInit<S>) => {
+  const session = Session(baseConfig).init();
 
-  const page = await Puppeteer();
+  return async () => {
+    const $t = await ScraperTool(session);
 
-  const $t = ScraperTools($s, page, itemData);
-
-  // SESSION WRAPPERS
-  const run = async <R, T extends (scraper: typeof $t) => Promise<R>>(
-    callback: T,
-  ) => {
-    const runResult = await $t.hooks.$$a(() =>
-      $s.setGlobalTimeout(async (cleanUp) => {
-        await $t.goToPage();
-        const result = await callback($t);
-        $s.end(false);
-        cleanUp();
-        return result;
-      }),
-    );
-
-    return runResult;
-  };
-
-  const afterAll = async <R, T extends (scraper: AfterAllTools) => Promise<R>>(
-    callback: T,
-  ) => {
-    infoMessage(t("session_actions.after_all"));
-
-    /**
-     * This function is meant to run only after the main actual run. Therefore, the store has already been
-     * ended and using the $$a function would cause this not to work (plus, it would be pointless)
-     */
-    const afterAllResult = tryCatch<ReturnType<T>>(
-      async () =>
-        await $s.setGlobalTimeout(async (cleanUp) => {
-          const result = await callback({
-            notify: $t.hooks.notify,
-            saveAsJson: $t.hooks.saveAsJson,
-            logMessage: $t.hooks.logMessage,
-          });
-
+    // SESSION WRAPPERS
+    const run = async <R, T extends (scraper: typeof $t) => Promise<R>>(
+      callback: T,
+    ) => {
+      const runResult = await $t.hooks.$$a(() =>
+        session.setGlobalTimeout(async (cleanUp) => {
+          await $t.init?.();
+          const result = await callback($t);
+          $t.abort(false);
           cleanUp();
           return result;
-        }, "afterAllTimeout"),
-    );
+        }),
+      );
 
-    return afterAllResult;
-  };
+      return runResult;
+    };
 
-  return {
-    run,
-    afterAll,
+    const afterAll = async <
+      R,
+      T extends (scraper: AfterAllTools) => Promise<R>,
+    >(
+      callback: T,
+    ) => {
+      infoMessage(t("session_actions.after_all"));
+
+      /**
+       * This function is meant to run only after the main actual run. Therefore, the store has already been
+       * ended and using the $$a function would cause this not to work (plus, it would be pointless)
+       */
+      const afterAllResult = tryCatch<ReturnType<T>>(
+        async () =>
+          await session.setGlobalTimeout(async (cleanUp) => {
+            const result = await callback({
+              notify: $t.hooks.notify,
+              saveAsJson: $t.hooks.saveAsJson,
+              logMessage: $t.hooks.logMessage,
+            });
+
+            cleanUp();
+            return result;
+          }, "afterAllTimeout"),
+      );
+
+      return afterAllResult;
+    };
+
+    return {
+      run,
+      afterAll,
+    };
   };
 };
 
-export default Scraper;
+export default scraper;
